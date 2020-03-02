bec25f7668c1166ff84bac739748a167929424c1
Shubhadeep Roychowdhury <shubhadeeproychowdhury@gmail.com>
2020-03-02 15:07:06 +0100

    preparing data for DL

--- prepare.py
+++ prepare.py
@@ -45,9 +45,21 @@ def main():
         
         segments = pd.Series(init_df["CONCATENATED_SEGMENTS"]).str.split(".")
         le = segments.str[0]
+        division = segments[1]
         acc_num = segments.str[2]
+        dept = segments[3]
+        area = segments[4]
+        merchandise_dept = segments[5]
+        inter_unit = segments[6]
+
         init_df["LEGAL_ENTITY"] = le.astype(float)
+        init_df["DIVISION"] = division.astype(str)
         init_df["ACCOUNT_NUMBER"] = acc_num.astype(str)
+        init_df["DEPARTMENT"] = dept.astype(str)
+        init_df["AREA"] = area.astype(str)
+        init_df["MARCHANDISE_DEPT"] = merchandise_dept.astype(str)
+        init_df["INTER_UNIT"] = inter_unit.astype(str)
+
         processed_df = init_df[init_df["ACCOUNT_NUMBER"].isin(INTERESTING_ACCS)]
         print(f"The processed df from {f} file {processed_df.shape} shape")
 
